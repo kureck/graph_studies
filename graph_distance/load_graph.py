8ef587748786c8e80253af0e1063c2efd661feec
ekureck <eric.kureck@walmart.com>
2015-07-03 18:50:33 -0300

    creates a digraph through a list of routes tuple

--- graph_distance/load_graph.py
+++ graph_distance/load_graph.py
@@ -1,8 +1,15 @@
-import networkx
+import networkx as nx
 
 
-def load_graph_from_file():
-    pass
+def load_graph_from_file(routes):
+    """ Transform a list of tuples into a graph object
+
+        :param routes: list of routes tuples
+        :returns: a networkx graph object
+    """
+    DG = nx.DiGraph()
+    DG.add_weighted_edges_from(routes)
+    return DG
 
 
 def load_file(file_name):
@@ -19,10 +26,21 @@ def load_file(file_name):
 
 
 def split_single_route(single_route):
+    """ Splits single route string from the "AB8" form into a ("A", "B", 8) tuple
+
+        :param single_route: route string in the form "AB8"
+        :returns: a tuple in the form ("A", "B", 8)
+
+    """
     decoupled = list(single_route)
     decoupled[-1] = int(decoupled[-1])
     return tuple(decoupled)
 
 
 def split_routes_input(raw_routes):
+    """ Creates a list of splited routes
+
+        :param raw_routes: list of raw_routes in the form ["AB8", "CD5"]
+        :returns: a list of tuples in the form [("A", "B", 8)]
+    """
     return [split_single_route(x) for x in raw_routes]
